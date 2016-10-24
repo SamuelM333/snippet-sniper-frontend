bc8d50d38e4413e854cd23c26a55f7e8b62d088e
Samuel Murillo <samuelmurillo333@gmail.com>
2016-10-24 02:27:18 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -1,30 +1,27 @@
 /* global $*/
 
-$('#scroll-1').click(function () {
-    $('body').animate({scrollTop: $('#carousel-section').offset().top + 'px'}, 1000);
-});
-
-$('#scroll-2').click(function () {
-    $('body').animate({scrollTop: $('#splitted-section').offset().top + 'px'}, 1000);
-});
-
-$('#scroll-3').click(function () {
-    $('body').animate({scrollTop: $('#main-section').offset().top + 'px'}, 1000);
-});
-
 $(document).ready(function(){
     $('.slider').slider({full_width: true});
     $('.carousel.carousel-slider').carousel({full_width: true});
-    $('#fullpage').fullpage();
-    // $('body').animate({scrollTop: $('#main-section').offset().top + 'px'}, 1000);
+    $(".button-collapse").sideNav();
+    $('#fullpage').fullpage({
+        anchors: ['firstPage', 'secondPage', 'thirdPage'],
+        navigation: true,
+        navigationPosition: 'left',
+        showActiveTooltip: true,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+        controlArrows: false,
+
+    });
     $(function () {
         $(".typed-text").typed({
-            strings: ["your text.", "your snippets.", "your code."],
+            strings: ["your text", "your snippets", "your code"],
             typeSpeed: 100,
             loop: true
         });
     });
-    
+
 });
 
 //add on resize
