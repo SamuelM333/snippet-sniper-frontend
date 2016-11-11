d6f5b159da8079274d4f646942af4f8f7e48a0d8
Samuel Murillo <samuelmurillo333@gmail.com>
2016-11-11 14:37:13 -0400

    Conditionally add styles to footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router'
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.sass']
+	selector: 'app-footer',
+	templateUrl: './footer.component.html',
+	styleUrls: ['./footer.component.sass']
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+	constructor(private router: Router) {
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.relativePos();
+	}
+
+	relativePos() {
+
+		if (this.router.url === '/snippet/view') {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
 
 }
