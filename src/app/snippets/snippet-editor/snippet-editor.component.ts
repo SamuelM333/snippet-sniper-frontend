ef1af3831035e32a9f0348f088ef98574f6aaa36
Samuel Murillo <samuelmurillo333@gmail.com>
2016-11-11 16:52:55 -0400

    Test Ace editor

--- src/app/snippets/snippet-editor/snippet-editor.component.ts
+++ src/app/snippets/snippet-editor/snippet-editor.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+declare var ace: any;
 
 @Component({
   selector: 'app-snippet-editor',
   templateUrl: './snippet-editor.component.html',
   styleUrls: ['./snippet-editor.component.sass']
 })
+
 export class SnippetEditorComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+	  var editor = ace.edit("editor");
+	  editor.setTheme("ace/theme/monokai");
+	  editor.getSession().setMode("ace/mode/javascript");
   }
 
 }
