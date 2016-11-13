4e3ca4816f1bf1f5dcaae9333b149aba10d3075d
Samuel Murillo <samuelmurillo333@gmail.com>
2016-11-13 12:20:13 -0400

    Get snippet content

--- src/app/snippets/snippet-editor/snippet-editor.component.ts
+++ src/app/snippets/snippet-editor/snippet-editor.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild, AfterViewInit, Input } from '@angular/core';
+import {Component, ViewChild, AfterViewInit} from '@angular/core';
 declare var ace: any;
 
 @Component({
@@ -10,45 +10,34 @@ declare var ace: any;
 export class SnippetEditorComponent implements AfterViewInit {
 
 	@ViewChild('editor') editor;
-	@Input() text: string = `function foo(items) {
+
+	title: string = '';
+	text: string = `function foo(items) {
 	var x = "All this is syntax highlighted";
-  	return x;
-}`;
+  	return x;\n}`;
 
 	languages = [
-		{id: 1, name: "Javascript", value: "javascript", icon: "assets/svg/js.svg"},
-		{id: 2, name: "Python", value: "python", icon: "assets/svg/python.svg"},
-		{id: 2, name: "C++", value: "c_cpp", icon: "assets/svg/c++.svg"}
+		{text: "Javascript", name: "javascript", icon: "assets/svg/js.svg"},
+		{text: "Python", name: "python", icon: "assets/svg/python.svg"},
+		{text: "C++", name: "c_cpp", icon: "assets/svg/c++.svg"}
 	];
-	selectedValue = this.languages[0];
 
-	constructor() { }
+	selectedValue = this.languages[0].name;
 
 	ngAfterViewInit() {
-		// $('select').material_select();
 		ace.config.set('basePath', 'assets/ace');
 		this.editor.setTheme("monokai");
-
-		// this.editor.getEditor().setOptions({
-		// 	enableBasicAutocompletion: true
-		// });
-
 		this.editor.setMode('javascript');
-
-		// this.editor.getEditor().commands.addCommand({
-		// 	name: "showOtherCompletions",
-		// 	bindKey: "Ctrl-.",
-		// 	exec: function (editor) { }
-		// });
-
 	}
 
-	getText(text: string){
-		console.log(text);
+	getSnippetDetails() {
+		console.log('title: ' + this.title);
+		console.log('language: ' + this.selectedValue);
+		console.log('code: \n' + this.editor.getEditor().getValue());
 	}
 
 	changeEditorLanguage(language) {
-		this.editor.setMode(language.value);
+		this.editor.setMode(language);
 	}
 
 }
