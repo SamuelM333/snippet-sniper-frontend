8475df245dbd39e1c25f7585721634f4d9e0ea87
Samuel Murillo <samuelmurillo333@gmail.com>
2016-11-13 13:54:37 -0400

    Add mobile nav

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-nav',
@@ -10,6 +11,7 @@ export class NavComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+	  $(".button-collapse").sideNav();
   }
 
 }
