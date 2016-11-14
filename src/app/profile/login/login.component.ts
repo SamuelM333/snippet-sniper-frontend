fc770a2a7ec413fc5293958054f3c36bb5b0b92a
Samuel Murillo <samuelmurillo333@gmail.com>
2016-11-13 23:08:38 -0400

    Handle login

--- src/app/profile/login/login.component.ts
+++ src/app/profile/login/login.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.sass']
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() { }
-
+export class LoginComponent {
+	
+	onSubmit(form: NgForm) {
+		console.log(form.value);
+	}
+	
 }
