89d581f56a4323570e113d3bc876fd4500fc6f98
Samuel Murillo <samuelmurillo333@gmail.com>
2016-11-13 23:09:03 -0400

    Handle sign up. Test form validation

--- src/app/profile/sign-up/sign-up.component.ts
+++ src/app/profile/sign-up/sign-up.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, ViewChild, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-sign-up',
-  templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.sass']
+	selector: 'app-sign-up',
+	templateUrl: './sign-up.component.html',
+	styleUrls: ['./sign-up.component.sass']
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() { }
-
+	@ViewChild('passwordRepeatLabel') passwordRepeatLabel;
+	@ViewChild('email') email;
+	@ViewChild('emailLabel') emailLabel;
+	gender: string = '';
+	password: string = '';
+	password_rpt: string = '';
+	valid: boolean = true;
+	
+	constructor() { }
+	
+	ngOnInit() { }
+	
+	onSubmit(form: NgForm) {
+		console.log(form.value);
+	}
+	
+	onKey(event: any) {
+		// console.log("pass: " + this.password);
+		// console.log("pass rpt: " + this.password_rpt);
+		// console.log(this.passwordRepeatLabel);
+		// console.log(this.email);
+		// console.log(this.emailLabel);
+		if (this.password === this.password_rpt && this.password !== '')
+			this.valid = true
+		else
+			this.valid = false
+		
+	}
+	
+	
 }
