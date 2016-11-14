9b321176da02c8bbf0bd142067e077d27af19f1d
Samuel Murillo <samuelmurillo333@gmail.com>
2016-11-14 15:34:04 -0400

    Some editor changes

--- src/app/snippets/snippet-editor/snippet-editor.component.ts
+++ src/app/snippets/snippet-editor/snippet-editor.component.ts
@@ -8,36 +8,37 @@ declare var ace: any;
 })
 
 export class SnippetEditorComponent implements AfterViewInit {
-	
+
 	@ViewChild('editor') editor;
-	
+
 	title: string = '';
+	// windowWidth = window.innerWidth;
 	text: string = `function foo(items) {
 	var x = "All this is syntax highlighted";
   	return x;\n}`;
-	
+
 	languages = [
-		{ text: "Javascript", name: "javascript", icon: "assets/svg/js.svg" },
-		{ text: "Python", name: "python", icon: "assets/svg/python.svg" },
-		{ text: "C++", name: "c_cpp", icon: "assets/svg/c++.svg" }
+		{text: "Javascript", name: "javascript", icon: "assets/svg/js.svg"},
+		{text: "Python", name: "python", icon: "assets/svg/python.svg"},
+		{text: "C++", name: "c_cpp", icon: "assets/svg/c++.svg"}
 	];
-	
+
 	selectedValue = this.languages[0].name;
-	
+
 	ngAfterViewInit() {
 		ace.config.set('basePath', 'assets/ace');
 		this.editor.setTheme("monokai");
 		this.editor.setMode('javascript');
+		this.editor.getEditor().setShowPrintMargin(false);
+		this.editor.getEditor().setAutoScrollEditorIntoView(true);
 	}
-	
+
 	getSnippetDetails() {
 		console.log('title: ' + this.title);
 		console.log('language: ' + this.selectedValue);
 		console.log('code: \n' + this.editor.getEditor().getValue());
 	}
-	
-	changeEditorLanguage(language) {
-		this.editor.setMode(language);
-	}
-	
+
+	changeEditorLanguage(language) { this.editor.setMode(language); }
+
 }
