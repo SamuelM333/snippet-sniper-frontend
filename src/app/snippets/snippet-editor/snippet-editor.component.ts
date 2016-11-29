b51e477923e858961c9bfa81a48aef38f1799ad4
Samuel Murillo <samuelmurillo333@gmail.com>
2016-11-29 18:12:37 -0400

    Add On rezise event on editor

--- src/app/snippets/snippet-editor/snippet-editor.component.ts
+++ src/app/snippets/snippet-editor/snippet-editor.component.ts
@@ -1,8 +1,8 @@
-import {Component, ViewChild, AfterViewInit, EventEmitter} from '@angular/core';
-import {MaterializeAction} from 'angular2-materialize';
+import { Component, ViewChild, AfterViewInit, EventEmitter } from '@angular/core';
+import { MaterializeAction } from 'angular2-materialize';
 
-declare var ace: any;
-declare var $: any;
+declare const ace: any;
+declare const $: any;
 
 @Component({
 	selector: 'app-snippet-editor',
@@ -11,54 +11,56 @@ declare var $: any;
 })
 
 export class SnippetEditorComponent implements AfterViewInit {
-
+	
 	@ViewChild('editor') editor;
 	modalActions = new EventEmitter<string|MaterializeAction>();
-
+	
 	title: string = '';
-	// windowWidth = window.innerWidth;
 	text: string = `function foo(items) {
 	var x = "All this is syntax highlighted";
   	return x;\n}`;
-
+	
 	languages = [
-		{text: "Javascript", name: "javascript", icon: "assets/svg/js.svg"},
-		{text: "Python", name: "python", icon: "assets/svg/python.svg"},
-		{text: "C++", name: "c_cpp", icon: "assets/svg/c++.svg"}
+		{ text: "Javascript", name: "javascript", icon: "assets/svg/js.svg" },
+		{ text: "Python", name: "python", icon: "assets/svg/python.svg" },
+		{ text: "C++", name: "c_cpp", icon: "assets/svg/c++.svg" }
 	];
-
+	
 	selectedValue = this.languages[0].name;
-
-
+	
+	
 	ngAfterViewInit() {
-
-		let topHeight: number = 0;
-		let footerHeight: number = $('#footer').outerHeight();
-		let documentHeight: number = $(document).outerHeight();
-
+		
 		ace.config.set('basePath', 'assets/ace');
 		this.editor.setTheme("monokai");
 		this.editor.setMode('javascript');
 		this.editor.getEditor().setShowPrintMargin(false);
 		this.editor.getEditor().setAutoScrollEditorIntoView(true);
-
-		setTimeout(() => {
-			topHeight = $('#nav').outerHeight() + $('#snippet-container').outerHeight();
-			$('#editor').height(documentHeight - topHeight - footerHeight + 1);
-		}, 1000);
-
+		
+		this.onResize();
+		
 	}
-
+	
 	getSnippetDetails() {
 		console.log('title: ' + this.title);
 		console.log('language: ' + this.selectedValue);
 		console.log('code: \n' + this.editor.getEditor().getValue());
 	}
-
+	
 	changeEditorLanguage(language) { this.editor.setMode(language); }
-
-	openModal() { this.modalActions.emit({action: "modal", params: ['open']}); }
-
-	closeModal() { this.modalActions.emit({action: "modal", params: ['close']}); }
-
+	
+	openModal() { this.modalActions.emit({ action: "modal", params: ['open'] }); }
+	
+	closeModal() { this.modalActions.emit({ action: "modal", params: ['close'] }); }
+	
+	onResize() {
+		
+		let topHeight: number = $('#nav').outerHeight() + $('#snippet-container').outerHeight();
+		let footerHeight: number = $('#footer').outerHeight();
+		let windowHeight: number = $(window).outerHeight();
+		
+		$('#editor').height(windowHeight - topHeight - footerHeight + 1);
+		
+	}
+	
 }
