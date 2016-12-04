bf184f57bfeb6cbb9f2d00f251fc31f79e1ac694
Samuel Murillo <samuelmurillo333@gmail.com>
2016-12-04 19:23:06 -0400

    Add insert user

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,31 +1,48 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from "@angular/http";
+import { Http, Response, Headers } from "@angular/http";
 import 'rxjs/Rx';
 
 @Injectable()
 export class ApiService {
 	
-	apiUrl = "http://127.0.0.1:5000";
+	// apiUrl = "http://127.0.0.1:5000";
 	// apiUrl = "http://api.snippet-sniper.samuelmurillo.me";
+	apiUrl = "http://snippetsniperphp-samuelm333.rhcloud.com";
 	
 	constructor(private http: Http) { }
 	
 	getSnippets() {
-		return this.http.get(this.apiUrl+'/snippet?embedded={"owner":1}').map(
+		return this.http.get(this.apiUrl + '/snippet?embedded={"owner":1}').map(
 			(response: Response) => response.json()
 		);
 	}
 	
 	getSnippetByID(id: String) {
-		return this.http.get(this.apiUrl+'/snippet/' +id + '?embedded={"owner":1}').map(
+		return this.http.get(this.apiUrl + '/snippet/' + id + '?embedded={"owner":1}').map(
 			(response: Response) => response.json()
 		);
 	}
 	
-	getUserByID(id: String) {
-		return this.http.get(this.apiUrl+'/user/' + id).map(
+	getUserByEmail(id: String) {
+		return this.http.get(this.apiUrl + '/user/' + id).map(
 			(response: Response) => response.json()
 		);
+	}
+	
+	insertUser(name: String, last_name: String, email: String, hashed_password: String) {
+		let headers = new Headers({ 'Content-Type': 'application/json' });
+		
+		let user = {
+			"name": name,
+			"last_name": last_name,
+			"email": email,
+			"password": hashed_password,
+			"date": new Date().toISOString().slice(0, 19).replace('T', ' ')
+		}
+		
+		return this.http.post(this.apiUrl + '/user', JSON.stringify(user), {
+			headers: headers
+		}).map((data: Response) => data.json());
 	}
 	
 }
