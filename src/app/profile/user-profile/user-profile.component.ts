c0548ae30fe79aa469d413fc900b97a679ab3a67
Samuel Murillo <samuelmurillo333@gmail.com>
2016-12-04 21:26:11 -0400

    Some testing

--- src/app/profile/user-profile/user-profile.component.ts
+++ src/app/profile/user-profile/user-profile.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+// declare const bcrypt: any = require('bcryptjs');
+
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.sass']
+	selector: 'app-user-profile',
+	templateUrl: './user-profile.component.html',
+	styleUrls: ['./user-profile.component.sass']
 })
 export class UserProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	
+	authUser = localStorage.getItem("authUser");
+	
+	ngOnInit() { if (this.authUser) this.authUser = JSON.parse(this.authUser); }
+	
 }
