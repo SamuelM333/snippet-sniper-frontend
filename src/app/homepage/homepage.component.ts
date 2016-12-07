b417d2918c9621cc6f8a6efc818ea6ef55df79bc
Samuel Murillo <samuelmurillo333@gmail.com>
2016-12-06 20:01:36 -0400

    Refactor code

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-homepage',
-	template: `
-		<app-floating-nav></app-floating-nav>
-		<app-slides></app-slides>
-	`
+	template: '<app-floating-nav></app-floating-nav> <app-slides></app-slides>'
 })
 
 export class HomepageComponent { }
