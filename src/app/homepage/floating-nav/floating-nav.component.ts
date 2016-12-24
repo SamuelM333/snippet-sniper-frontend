2ea80a93821f78f4ef0c5481ee2b55217320d344
Samuel Murillo <samuelmurillo333@gmail.com>
2016-12-23 20:40:33 -0400

    Update angular cli

--- src/app/homepage/floating-nav/floating-nav.component.ts
+++ src/app/homepage/floating-nav/floating-nav.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-floating-nav',
-  templateUrl: 'floating-nav.component.html',
-  styleUrls: ['floating-nav.component.sass']
+  templateUrl: './floating-nav.component.html',
+  styleUrls: ['./floating-nav.component.sass']
 })
 export class FloatingNavComponent { }
