609d3f81978e2d1d3438135ecea638d97274015a
Samuel Murillo <samuelmurillo333@gmail.com>
2016-12-25 14:54:35 -0400

    Add On init hook

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-homepage',
     template: '<app-floating-nav></app-floating-nav> <app-slides></app-slides>'
 })
 
-export class HomepageComponent {}
+export class HomepageComponent implements OnInit {
+
+    ngOnInit(){
+
+    }
+
+}
