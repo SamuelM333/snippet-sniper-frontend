7456b7e03e7d39ea3fe45485433104960811199e
Samuel Murillo <samuelmurillo333@gmail.com>
2016-12-25 20:36:59 -0400

    My account tab

--- src/app/profile/user-profile/user-profile.component.ts
+++ src/app/profile/user-profile/user-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 declare const $: any;
+declare const Materialize: any;
 
 @Component({
     selector: 'app-user-profile',
@@ -9,22 +11,34 @@ declare const $: any;
 })
 export class UserProfileComponent implements OnInit, AfterViewInit {
 
-    authUser = localStorage.getItem('authUser');
+    authUser = JSON.parse(localStorage.getItem('authUser'));
+    password: string = '';
+    password_rpt: string = '';
+    valid: boolean = true;
 
     ngOnInit() {
-        if (this.authUser) { this.authUser = JSON.parse(this.authUser); }
-
         $('ul.tabs').tabs();
-
+        $('ul.tabs').tabs('select_tab', 'tab3');
     }
 
-    ngAfterViewInit() {
-        // $('ul.tabs').tabs('select_tab', 'tab1');
-    }
+    ngAfterViewInit() { Materialize.updateTextFields(); }
 
     onResize() {
         $('ul.tabs').tabs();
-        $('ul.tabs').tabs('select_tab', 'tab1');
+        $('ul.tabs').tabs('select_tab', 'tab3');
+    }
+
+    changeProfilePicture() {
+        console.log('click on photo');
+    }
+
+    onSubmitForm(form: NgForm) {
+        console.log(form);
+    }
+
+    onKey(event: any) {
+        this.valid = false;
+        if (this.password === this.password_rpt && this.password !== '') { this.valid = true; }
 
     }
 
