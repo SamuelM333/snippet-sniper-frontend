7c0200304e42c18bbd93fe7585808e2cbf67ed00
Samuel Murillo <samuelmurillo333@gmail.com>
2016-12-28 14:35:32 -0400

    Validate not logged user

--- src/app/profile/user-profile/user-profile.component.ts
+++ src/app/profile/user-profile/user-profile.component.ts
@@ -1,5 +1,8 @@
-import { Component, AfterViewInit, ViewChild } from '@angular/core';
-import { NgForm } from "@angular/forms";
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+import { ApiService } from '../../api.service';
 
 declare const $: any;
 declare const Materialize: any;
@@ -9,18 +12,32 @@ declare const Materialize: any;
     templateUrl: './user-profile.component.html',
     styleUrls: ['./user-profile.component.sass']
 })
-export class UserProfileComponent implements AfterViewInit {
-    
+export class UserProfileComponent implements OnInit, AfterViewInit {
+
     authUser = JSON.parse(localStorage.getItem('authUser'));
     profilePicture: File = null;
-    name = this.authUser.name;
-    last_name = this.authUser.last_name;
-    email = this.authUser.email;
+    name: string = '';
+    last_name: string = '';
+    email: string = '';
     password: string = '';
     password_rpt: string = '';
+    admin: number = 0;
     updateInformationEnabled: boolean = false;
     changePasswordEnabled: boolean = false;
-    
+
+    constructor(private router: Router, private apiService: ApiService) { }
+
+    ngOnInit() {
+        if (this.authUser) {
+            this.name = this.authUser.name;
+            this.last_name = this.authUser.last_name;
+            this.email = this.authUser.email;
+            this.admin = this.authUser.admin;
+        } else {
+            this.router.navigateByUrl('/login');
+        }
+    }
+
     ngAfterViewInit() { this.onResize(); }
 
     onResize() {
@@ -31,26 +48,26 @@ export class UserProfileComponent implements AfterViewInit {
     onSubmitForm(form: NgForm) {
         console.log(form);
     }
-    
-    fileChangeEvent(fileInput: any){
+
+    fileChangeEvent(fileInput: any) {
         this.profilePicture = fileInput.target.files[0];
         console.log(this.profilePicture);
     }
 
     passwordFormUpdated(event: any) {
-        if (this.password === this.password_rpt && this.password !== '') { 
-            this.changePasswordEnabled = true; 
+        if (this.password === this.password_rpt && this.password !== '') {
+            this.changePasswordEnabled = true;
         } else {
             this.changePasswordEnabled = false;
         }
     }
-    
+
     profileInformationFormUpdated(event: any) {
-        if (this.name !== this.authUser.name || this.last_name !== this.authUser.last_name || this.email !== this.authUser.email) { 
-                this.updateInformationEnabled = true;
+        if (this.name !== this.authUser.name || this.last_name !== this.authUser.last_name || this.email !== this.authUser.email) {
+            this.updateInformationEnabled = true;
         } else {
             this.updateInformationEnabled = false;
         }
     }
-    
+
 }
