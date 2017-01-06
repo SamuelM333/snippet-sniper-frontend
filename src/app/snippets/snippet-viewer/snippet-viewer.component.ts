3cb2545509e9cd51e37443f3481d71350d799456
Samuel Murillo <samuelmurillo333@gmail.com>
2017-01-06 18:22:29 -0400

    Redesign snippet viewer

--- src/app/snippets/snippet-viewer/snippet-viewer.component.ts
+++ src/app/snippets/snippet-viewer/snippet-viewer.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, AfterViewChecked } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, AfterViewChecked, EventEmitter } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
-import { ApiService } from '../../api.service';
-import { Snippet, serializeSnippet } from '../snippet';
+import { ApiService, apiUrl } from '../../api.service';
+import { Snippet } from '../snippet';
+import { MaterializeAction } from "angular2-materialize";
 
 declare const Prism: any;
 
@@ -13,23 +14,35 @@ declare const Prism: any;
 })
 export class SnippetViewerComponent implements OnInit, AfterViewChecked {
 
+    url = apiUrl;
+    loading: boolean = false;
     idSnippet: string;
     snippet: Snippet;
     authUser = JSON.parse(localStorage.getItem('authUser'));
+
+    shareModal = new EventEmitter<string|MaterializeAction>();
+    editModal = new EventEmitter<string|MaterializeAction>();
+    deleteModal = new EventEmitter<string|MaterializeAction>();
 
     constructor(private activatedRoute: ActivatedRoute, private apiService: ApiService) {
         this.idSnippet = activatedRoute.snapshot.params['id'];
     }
 
     ngOnInit() {
+        this.loading = true;
         if (this.authUser) {
-            console.log(this.authUser);
             this.apiService.getSnippetByID(this.idSnippet, this.authUser.email, this.authUser.password).subscribe(
-                data => this.snippet = data
+                data => {
+                    this.snippet = data;
+                    this.loading = false;
+                }
             );
         } else {
             this.apiService.getSnippetByID(this.idSnippet, null, null).subscribe(
-                data => this.snippet = data
+                data => {
+                    this.snippet = data;
+                    this.loading = false;
+                }
             );
         }
 
@@ -37,5 +50,11 @@ export class SnippetViewerComponent implements OnInit, AfterViewChecked {
 
     // Called a lot, fix
     ngAfterViewChecked() { Prism.highlightAll(); }
+
+    openShareModal() { this.shareModal.emit({ action: 'modal', params: ['open'] }); }
+    openEditModal() { this.editModal.emit({ action: 'modal', params: ['open'] }); }
+    openDeleteModal() { this.deleteModal.emit({ action: 'modal', params: ['open'] }); }
+
+    closeModal() { this.shareModal.emit({ action: 'modal', params: ['close'] }); }
 
 }
