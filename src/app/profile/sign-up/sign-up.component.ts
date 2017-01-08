8eb68cb13d1ad929a3ad9f361ca625fc2da899ff
Samuel Murillo <samuelmurillo333@gmail.com>
2017-01-07 20:53:48 -0400

    Validate unique email

--- src/app/profile/sign-up/sign-up.component.ts
+++ src/app/profile/sign-up/sign-up.component.ts
@@ -1,10 +1,10 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-
 import { ApiService } from '../../api.service';
-
 const bcrypt = require('bcryptjs');
+
+declare const Materialize: any;
 
 @Component({
     selector: 'app-sign-up',
@@ -19,32 +19,36 @@ export class SignUpComponent implements OnInit {
     password: string = '';
     password_rpt: string = '';
     valid: boolean = true;
+    loading: boolean = false;
 
     constructor(private router: Router, private apiService: ApiService) { }
 
     ngOnInit() { if (localStorage.getItem('authUser')) { this.router.navigateByUrl('/profile'); } }
 
     onSubmit(form: NgForm) {
-        let hashed_password = bcrypt.hashSync(form.value.password, 10);
-        this.apiService.insertUser(form.value.name, form.value.last_name, form.value.email,
+        this.loading = true;
+        let hashed_password = bcrypt.hashSync(form.value.password.trim(), 10);
+        this.apiService.insertUser(form.value.name.trim(), form.value.last_name.trim(), form.value.email.trim(),
             hashed_password).subscribe(
             data => {
-                if (data._status === 'OK') {
-                    let authUser = {
-                        'id': null,
-                        'name': form.value.name,
-                        'last_name': form.value.last_name,
-                        'email': form.value.email,
-                        'password': hashed_password,
-                        'picture': {
-                            'url': null,
-                            'uploaded': null,
-                        },
-                        'admin': 0,
-                    };
-                    localStorage.setItem('authUser', JSON.stringify(authUser));
-                    this.router.navigateByUrl('/profile');
-                } else { console.log('Error on sign up'); }
+                let authUser = {
+                    'id': data.idUser,
+                    'name': form.value.name.trim(),
+                    'last_name': form.value.last_name.trim(),
+                    'email': form.value.email.trim(),
+                    'password': hashed_password,
+                    'picture': {
+                        'url': null,
+                        'uploaded': null,
+                    },
+                    'admin': 0,
+                };
+                localStorage.setItem('authUser', JSON.stringify(authUser));
+                this.router.navigateByUrl('/profile');
+            },
+            error => {
+                Materialize.toast('Email already in use', 4000);
+                this.loading = false;
             }
         );
 
@@ -52,11 +56,6 @@ export class SignUpComponent implements OnInit {
 
     onKey(event: any) {
         this.valid = false;
-        // console.log('pass: ' + this.password);
-        // console.log('pass rpt: ' + this.password_rpt);
-        // console.log(this.passwordRepeatLabel);
-        // console.log(this.email);
-        // console.log(this.emailLabel);
         if (this.password === this.password_rpt && this.password !== '') { this.valid = true; }
 
     }
