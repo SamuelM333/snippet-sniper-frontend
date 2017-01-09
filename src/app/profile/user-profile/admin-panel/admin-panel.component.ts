f67a7697f0a095114ee31ec4a23a2dfe2d685247
Samuel Murillo <samuelmurillo333@gmail.com>
2017-01-08 21:47:25 -0400

    Add admin panel

--- src/app/profile/user-profile/admin-panel/admin-panel.component.ts
+++ src/app/profile/user-profile/admin-panel/admin-panel.component.ts
@@ -1,15 +1,123 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../api.service';
+import { NgForm } from '@angular/forms';
+const bcrypt = require('bcryptjs');
+declare const Materialize: any;
+
 
 @Component({
-  selector: 'app-admin-panel',
-  templateUrl: './admin-panel.component.html',
-  styleUrls: ['./admin-panel.component.sass']
+    selector: 'app-admin-panel',
+    templateUrl: './admin-panel.component.html',
+    styleUrls: ['./admin-panel.component.sass']
 })
 export class AdminPanelComponent implements OnInit {
 
-  constructor() { }
+    publicSnippetCount: number = 0;
+    activeState: boolean;
+    adminState: boolean;
+    password: string = '';
+    password_rpt: string = '';
+    valid: boolean = true;
+    loading: boolean = false;
 
-  ngOnInit() {
-  }
+    authUser = JSON.parse(localStorage.getItem('authUser'));
+
+    constructor(private apiService: ApiService) { }
+
+    ngOnInit() {
+        this.apiService.getSnippets().subscribe(
+            data => {
+                this.publicSnippetCount = data._meta.total
+            },
+            error => {
+                Materialize.toast('Email already in use', 4000);
+                this.loading = false;
+            }
+        );
+    }
+
+    onSubmitNewUser(form: NgForm) {
+        this.loading = true;
+        let hashed_password = bcrypt.hashSync(form.value.password.trim(), 10);
+        this.apiService.insertUser(form.value.name.trim(), form.value.last_name.trim(), form.value.email.trim(),
+            hashed_password).subscribe(
+            data => {
+                // Redirect to user profile?
+                // this.router.navigateByUrl('/profile/data.idUser');
+                // form.reset();
+            },
+            error => {
+                Materialize.toast('Email already in use', 4000);
+                this.loading = false;
+            }
+        );
+    }
+
+    onKey(event: any) {
+        this.valid = false;
+        if (this.password === this.password_rpt && this.password !== '') { this.valid = true; }
+    }
+
+    onSubmitDeleteSnippet(form: NgForm) {
+        if (this.authUser) {
+            this.apiService.deleteSnippet(form.value.snippet_id, this.authUser.email, this.authUser.password).subscribe(
+                data => {
+                    Materialize.toast('Snippet deleted', 4000);
+                    form.reset();
+                },
+                error => {
+                    if (error.status === 404) {
+                        Materialize.toast('Snippet not found', 4000);
+                    } else if (error.status === 401) {
+                        Materialize.toast('Not allowed to do that', 4000);
+                    } else {
+                        Materialize.toast('Error', 4000);
+                    }
+                }
+            );
+        }
+    }
+
+    onSubmitDeleteUser(form: NgForm) {
+        if (this.authUser) {
+            this.apiService.changeUserState(form.value.email, form.value.state, this.authUser.email, this.authUser.password)
+                .subscribe(
+                    data => {
+                        Materialize.toast('User active state changed', 4000);
+                        form.reset();
+                    },
+                    error => {
+                        if (error.status === 404) {
+                            Materialize.toast('User not found', 4000);
+                        } else if (error.status === 401) {
+                            Materialize.toast('Not allowed to do that', 4000);
+                        } else {
+                            Materialize.toast('Error', 4000);
+                        }
+                    }
+                );
+        }
+    }
+
+    onSubmitMakeAdmin(form: NgForm) {
+        if (this.authUser) {
+            this.apiService.changeUserAdmin(form.value.email, form.value.state, this.authUser.email, this.authUser.password)
+                .subscribe(
+                    data => {
+                        Materialize.toast('User admin state changed', 4000);
+                        form.reset();
+                    },
+                    error => {
+                        if (error.status === 404) {
+                            Materialize.toast('User not found', 4000);
+                        } else if (error.status === 401) {
+                            Materialize.toast('Not allowed to do that', 4000);
+                        } else {
+                            Materialize.toast('Error', 4000);
+                        }
+                    }
+                );
+        }
+    }
 
 }
