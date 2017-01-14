f67dc7f991dfb7a353261b1b4ebe9f64b47f02ac
Samuel Murillo <samuelmurillo333@gmail.com>
2017-01-14 18:33:14 -0400

    Redirect to /snippets if logged

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-homepage',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 
 export class HomepageComponent implements OnInit {
 
-    ngOnInit() { }
+    constructor(private router: Router) { }
+
+    ngOnInit() {
+        if (localStorage.getItem('authUser')) {
+            this.router.navigateByUrl('/snippets');
+        }
+    }
 
 }
