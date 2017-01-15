9bcd6c31c6fa2c2f9d93fae3a4bac92a2f2dde3e
Samuel Murillo <samuelmurillo333@gmail.com>
2017-01-14 21:49:32 -0400

    Fix sidenav bug on route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+
+declare const $:any;
 
 @Component({
     selector: 'app-root',
     template: '<router-outlet></router-outlet>',
 })
-export class AppComponent { }
+export class AppComponent {
+    constructor(private router: Router) {
+        router.events.subscribe((val) => {
+            $('.button-collapse').sideNav('hide');
+        });
+    }
+}
